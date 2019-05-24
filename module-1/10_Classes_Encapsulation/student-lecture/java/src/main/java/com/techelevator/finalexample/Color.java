74831a5ba5b46a1c6c442a07e254626ac331c46b
Steve Carmichael <steve@techelevator.com>
2019-05-24 07:56:06 -0400

    added encapsulation

---
+++ module-1/10_Classes_Encapsulation/student-lecture/java/src/main/java/com/techelevator/finalexample/Color.java
@@ -0,0 +1,12 @@
+package com.techelevator.finalexample;
+
+public class Color {
+
+	    //defining constants
+	    public static final int YELLOW = 1;
+	    public static final int ORANGE = 2;
+	    public static final int BLUE = 3;	    
+	    public static final int RED = 4;
+	    public static final int GREEN = 5;
+	   
+}
