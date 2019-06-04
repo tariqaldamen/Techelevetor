22139dfb48c9e48d1e7bd3b7376ea28020a88844
Tariq Aldamen <Tariq.alda@gmail.com>
2019-06-03 20:44:12 -0400

    new change

--- module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -1,5 +1,18 @@
 package com.techelevator;
 
 public class KataFizzBuzz {
+	
+	
+public String FizzBuzz(int number)
+{
+//	if(number % 3 == 0)
+//	{
+//		return "Fizz";
+//	}
+	
+	Integer convertIntToInteger= (Integer)number;
+	String converToString = convertIntToInteger.toString();
+	return converToString;
+}
 
 }
--- module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -4,5 +4,15 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class KataFizzBuzzTest {
+	
+	
 
-}
+	@Test
+	public void StringCalculatorTest() {
+
+		KataFizzBuzz test = new KataFizzBuzz();
+		Assert.assertEquals("1", test.FizzBuzz(1));
+		Assert.assertEquals(("2"), test.FizzBuzz(2));
+	
+	}
+	}
